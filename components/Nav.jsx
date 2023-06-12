4b8963f0b7cf4d62dda7d6e93d52b26de9fb83ce
HamzaRaza786 <hamza.raza2015@outlook.com>
2023-06-12 14:46:17 +0500

    reformat code

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,27 +2,29 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
-  const {data:session} = useSession();
+  const { data: session } = useSession();
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState();
   useEffect(() => {
-    const setUpProviders = async() => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
-    }
+    };
     setUpProviders();
   }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/assets/images/logo.svg" 
-                alt="hello" 
-                width={30}
-                height={30}
-                className="obj-container"/>
+        <Image
+          src="/assets/images/logo.svg"
+          alt="hello"
+          width={30}
+          height={30}
+          className="obj-container"
+        />
         <p className="logo-text">Proomptopia</p>
       </Link>
       <div className="sm:flex hidden">
@@ -31,33 +33,37 @@ const Nav = () => {
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
-            <button type="button" 
-              onClick={signOut} className = "outline_btn">SignOut
+            <button type="button" onClick={signOut} className="outline_btn">
+              SignOut
             </button>
-          <Link href="/profile">
-            <Image src={session?.user.image}
-                  alt = "profile" width={37} height={37} 
-                  className = "rounded-full"/>
-          </Link>
-
-            
+            <Link href="/profile">
+              <Image
+                src={session?.user.image}
+                alt="profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
           </div>
-        ):
-        <>
-          {providers &&
-            Object.values(providers).map(
-              (provider) => (
-                <button type="button" 
-                key={provider.name}
-                onClick={() => signIn(provider.id)}
-                className='black_btn'>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
                   Sign In
                 </button>
-            ))}
-        </>}
+              ))}
+          </>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
